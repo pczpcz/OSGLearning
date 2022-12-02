5b202d566fd7d5264610c00e000c2212ee6257dc
zcp <1099311614@qq.com>
2022-12-02 21:48:39 +0800

    Lession11: 拖拽功能(拖拽器-选择器-CommandManager)

--- Lession_11_GUIManipulator/Lession_11_GUIManipulator/Lession_1_Start/Lession_1_Start.cpp
+++ Lession_11_GUIManipulator/Lession_11_GUIManipulator/Lession_1_Start/Lession_1_Start.cpp
@@ -18,20 +18,71 @@
 #include <osg/Camera>
 #include <osgViewer/CompositeViewer>
 #include <osgGA/GUIEventHandler>
+#include <osgManipulator/TranslateAxisDragger>
+#include <osgManipulator/CommandManager>
+#include <osgUtil/LineSegmentIntersector>
 
 class UserGUIEventHandler : public osgGA::GUIEventHandler 
 {
 public:
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object* obj, osg::NodeVisitor* nv) 
 	{
-		osgViewer::Viewer* viewer = dynamic_cast<osgViewer::Viewer *>(&aa);
-		if (!viewer) 
+		osgViewer::Viewer* viewer = dynamic_cast<osgViewer::Viewer*>(&aa);
+		if (!viewer)
 		{
 			return false;
 		}
 	
 		switch (ea.getEventType())
 		{
+		case osgGA::GUIEventAdapter::PUSH: 
+			{
+				_pointer.reset();
+				osgUtil::LineSegmentIntersector::Intersections hits;
+				if (viewer->computeIntersections(ea.getX(), ea.getY(), hits))
+				{
+					_pointer.setCamera(viewer->getCamera());
+					_pointer.setMousePosition(ea.getX(), ea.getY());
+
+					//获取所有有交集的对象
+					for (auto ittr = hits.begin(); ittr != hits.end(); ++ittr) 
+					{
+						_pointer.addIntersection(ittr->nodePath, ittr->getLocalIntersectPoint());
+					}
+
+					//选取第一个交集对象，其他对象不选取???, 用front没太看明白，drager一定是第一个吗？？？
+					for (auto ittr = _pointer._hitList.front().first.begin(); ittr != _pointer._hitList.front().first.end(); ++ittr) 
+					{
+						osgManipulator::Dragger* dragger = dynamic_cast<osgManipulator::Dragger*>(*ittr);
+						if (dragger)
+						{
+							dragger->handle(_pointer, ea, aa);
+							_activeDragger = dragger;
+							break;
+						}
+					}
+				}
+			}
+			break;
+		case osgGA::GUIEventAdapter::DRAG:
+		case osgGA::GUIEventAdapter::RELEASE: 
+			{
+				if (_activeDragger) 
+				{
+					_pointer._hitIter = _pointer._hitList.begin();
+					_pointer.setCamera(viewer->getCamera());
+					_pointer.setMousePosition(ea.getX(), ea.getY());	//传递拖动过程中坐标位置变化
+					_activeDragger->handle(_pointer, ea,aa);
+				}
+				
+				//osgGA::GUIEventAdapter::DRAG也会进来，固由此判断
+				if (ea.getEventType() == osgGA::GUIEventAdapter::RELEASE)
+				{
+					_activeDragger = NULL;
+					_pointer.reset();
+				}
+			}
+			break;
 		case osgGA::GUIEventAdapter::KEYDOWN: 
 			{
 				if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Space) 
@@ -68,6 +119,9 @@ public:
 		}
 		return false;
 	}
+protected:
+	osgManipulator::Dragger* _activeDragger;
+	osgManipulator::PointerInfo _pointer;
 };
 
 int main()
@@ -75,15 +129,30 @@ int main()
 	osg::ref_ptr<osg::Switch> root = new osg::Switch;
 	osg::ref_ptr<osg::Node> node0 = osgDB::readNodeFile("osgData/cessna.osg");
 	osg::ref_ptr<osg::Node> node1 = osgDB::readNodeFile("osgData/cessnafire.osg");
+	//root->addChild(node0.get(), true);
+	//root->addChild(node1.get(), true);
 
-	root->addChild(node0.get(), true);
-	root->addChild(node1.get(), true);
+	//拖拽功能
+	osg::ref_ptr<osg::Node> axis = osgDB::readNodeFile("osgData/axes.osgt");
+	osg::ref_ptr<osgManipulator::Selection> selection = new osgManipulator::Selection;
+	selection->addChild(node0.get());
 
-	osg::ref_ptr<UserGUIEventHandler> handler = new UserGUIEventHandler;
+	//操控的几何原理还不太懂？？？
+	float scale = node0->getBound().radius() * 1.6;
+	osg::ref_ptr<osgManipulator::TranslateAxisDragger> dragger = new osgManipulator::TranslateAxisDragger;
+	dragger->setupDefaultGeometry();
+	dragger->setMatrix(osg::Matrix::scale(scale, scale, scale) * osg::Matrix::translate(node0->getBound().center()));
+
+	root->addChild(selection.get());
+	root->addChild(dragger.get());
+	root->addChild(axis.get());
+
+	osg::ref_ptr<osgManipulator::CommandManager> cmdManager = new osgManipulator::CommandManager;
+	cmdManager->connect(*dragger, *selection);
 
 	osgViewer::Viewer viewer;
 	viewer.setSceneData(root.get());
-	viewer.addEventHandler(handler.get());
+	viewer.addEventHandler(new UserGUIEventHandler);
 	return viewer.run();
 }
 
