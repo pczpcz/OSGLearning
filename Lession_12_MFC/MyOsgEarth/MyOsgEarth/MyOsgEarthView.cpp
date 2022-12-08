47a3f9a7b3faff13f1139ed2c750a76425c92106
zcp <1099311614@qq.com>
2022-12-08 22:40:59 +0800

    Lession12: osg&MFC联合开发

---
+++ Lession_12_MFC/MyOsgEarth/MyOsgEarth/MyOsgEarthView.cpp
@@ -0,0 +1,180 @@
+﻿
+// MyOsgEarthView.cpp: CMyOsgEarthView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MyOsgEarth.h"
+#endif
+
+#include "MyOsgEarthDoc.h"
+#include "MyOsgEarthView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+osg::ref_ptr<osgViewer::Viewer> g_refptrViewer = new osgViewer::Viewer;
+bool g_bFinished = true;
+
+// CMyOsgEarthView
+
+IMPLEMENT_DYNCREATE(CMyOsgEarthView, CView)
+
+BEGIN_MESSAGE_MAP(CMyOsgEarthView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMyOsgEarthView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_WM_CREATE()
+END_MESSAGE_MAP()
+
+void CMyOsgEarthView::Render(void *)
+{
+	while (!(g_refptrViewer->done()))
+	{
+		g_refptrViewer->frame();
+	}
+	g_bFinished = true;
+}
+
+// CMyOsgEarthView 构造/析构
+CMyOsgEarthView::CMyOsgEarthView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMyOsgEarthView::~CMyOsgEarthView()
+{
+}
+
+BOOL CMyOsgEarthView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMyOsgEarthView 绘图
+
+void CMyOsgEarthView::OnDraw(CDC* /*pDC*/)
+{
+	CMyOsgEarthDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMyOsgEarthView 打印
+
+
+void CMyOsgEarthView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMyOsgEarthView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMyOsgEarthView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMyOsgEarthView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CMyOsgEarthView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMyOsgEarthView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMyOsgEarthView 诊断
+
+#ifdef _DEBUG
+void CMyOsgEarthView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMyOsgEarthView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMyOsgEarthDoc* CMyOsgEarthView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMyOsgEarthDoc)));
+	return (CMyOsgEarthDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMyOsgEarthView 消息处理程序
+
+
+int CMyOsgEarthView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	osg::ref_ptr<osg::Referenced> windata = new osgViewer::GraphicsWindowWin32::WindowData(m_hWnd);
+
+	osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+	traits->x = 0;
+	traits->y = 0;
+	CRect rect;
+	GetWindowRect(&rect);
+	traits->width = rect.Width();
+	traits->height = rect.Height();
+	traits->windowDecoration = false;
+	traits->doubleBuffer = true;
+	traits->inheritedWindowData = windata;
+
+	osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
+
+	osg::ref_ptr<osg::Camera> camera = new osg::Camera;
+	camera->setGraphicsContext(gc);
+	camera->setViewport(new osg::Viewport(0, 0, traits->width, traits->height));
+
+	if (!g_refptrViewer) 
+	{
+		g_refptrViewer = new osgViewer::Viewer;
+	}
+	g_refptrViewer->addSlave(camera.get());
+	g_refptrViewer->setSceneData(osgDB::readNodeFile("osgData/cow.osg"));
+	g_refptrViewer->setCameraManipulator(new osgGA::TrackballManipulator);
+
+	g_refptrViewer->setThreadingModel(osgViewer::Viewer::SingleThreaded);
+	
+	g_bFinished = false;
+	_beginthread(Render, 0, NULL);
+
+	return 0;
+}
---
+++ Lession_12_MFC/MyOsgEarth/MyOsgEarth/MyOsgEarthView.h
@@ -0,0 +1,61 @@
+﻿
+// MyOsgEarthView.h: CMyOsgEarthView 类的接口
+//
+
+#pragma once
+
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osgViewer/api/win32/GraphicsWindowWin32>
+#include <osgGA/TrackballManipulator>
+
+class CMyOsgEarthView : public CView
+{
+protected: // 仅从序列化创建
+	CMyOsgEarthView() noexcept;
+	DECLARE_DYNCREATE(CMyOsgEarthView)
+
+// 特性
+public:
+	CMyOsgEarthDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMyOsgEarthView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+
+public:
+	static void CMyOsgEarthView::Render(void*);
+};
+
+#ifndef _DEBUG  // MyOsgEarthView.cpp 中的调试版本
+inline CMyOsgEarthDoc* CMyOsgEarthView::GetDocument() const
+   { return reinterpret_cast<CMyOsgEarthDoc*>(m_pDocument); }
+#endif
+
